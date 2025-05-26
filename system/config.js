b5493f22f54cf91a04ccb5ca166e796287146a15
windy <awswindy7@gmail.com>
2025-05-26 12:26:45 +0700

    Citato

---
+++ system/config.js
@@ -0,0 +1,13 @@
+///Script Ini Free, Jika Ingin Membeli No Enc Bisa Hubungi Owner, Wa : 6283142078784 Tele : @ZauzetBusy
+//========ZAUZET========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6281553926503"] 
+global.namabot = 'SecretMystery'
+//======================
+global.mess = { 
+owner: 'waduhh!, lu bukan owner gw bg',
+premium: 'anda bukan user premium',
+succes: 'done bang'
+}
+//======================
